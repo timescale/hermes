5c0864e3b136accb1de2071921b0fc376d8170a7
Justin Murray <justin@tigerdata.com>
2026-02-20 11:35:38 -0500

    fix domain for install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # hermes installer - Install script for macOS/Linux
-# Usage: curl -fsSL https://hermes.tiger.build | sh
+# Usage: curl -fsSL https://hermes.ghost.build | sh
 
 set -e
 
